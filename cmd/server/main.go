2d0ad76c73831b1905afb106a04e52cd151d6799
AhmetMuratAcar <muratacar.dev@gmail.com>
2026-07-01 14:13:12 -0700

    lesson 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
+	}
+
+	connection, err := amqp.Dial(os.Getenv("AMQP_URL"))
+	if err != nil {
+		log.Fatalf("Error connecting to AMQP: %v", err)
+	}
+	defer connection.Close()
+
+	fmt.Println("Connection to AMQP successful")
+
+	// wait for close signal
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutdown signal received, program shutting down")
 }
